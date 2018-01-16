947d3b933397de77625d214815ac8184f22c7750
William Woodruff <william@tuffbizz.com>
2018-01-16 10:43:49 -0500

    spec: Add stub specs for XDo::Window

---
+++ spec/window_spec.cr
@@ -0,0 +1,15 @@
+require "./spec_helper"
+
+describe XDo::Window do
+  pending "#mouse_move" do
+  end
+
+  pending "#mouse_down" do
+  end
+
+  pending "#mouse_up" do
+  end
+
+  pending "#click" do
+  end
+end
